585124d1b1b85d2488fb1fe07257b4047886b3c7
Anna Kozlovskaia <amorelleth@gmail.com>
2024-06-07 16:29:45 +0200

    feat: backend: setup cors, edit tsconfig

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,43 +1,46 @@
-// index.js
-const express = require('express');
-const https = require('https');
+import express from "express";
+import https from "https";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
-// Function to fetch data from SpaceX API using https module
 const fetchLaunches = () => {
-    return new Promise((resolve, reject) => {
-        https.get('https://api.spacexdata.com/v4/launches', (res: any) => {
-            let data = '';
+  return new Promise((resolve, reject) => {
+    https
+      .get("https://api.spacexdata.com/v5/launches", (res) => {
+        let data = "";
 
-            // A chunk of data has been received.
-            res.on('data', (chunk: any) => {
-                data += chunk;
-            });
-
-            // The whole response has been received. Print out the result.
-            res.on('end', () => {
-                resolve(JSON.parse(data));
-            });
-
-        }).on('error', (err: any) => {
-            reject(err);
+        res.on("data", (chunk) => {
+          data += chunk;
         });
-    });
+
+        res.on("end", () => {
+          resolve(JSON.parse(data));
+        });
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
 };
 
-// Endpoint to fetch launches from SpaceX API
-app.get('/api/launches', async (req: any, res: any) => {
-    try {
-        const launches = await fetchLaunches();
-        res.json(launches);
-    } catch (error) {
-        res.status(500).send('Error fetching launches');
-    }
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.get("/api/launches", async (_, res) => {
+  try {
+    const launches = await fetchLaunches();
+    res.json(launches);
+  } catch (error) {
+    res.status(500).send("Error fetching launches");
+  }
 });
 
-// Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
