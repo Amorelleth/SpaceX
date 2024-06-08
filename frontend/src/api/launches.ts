986e1b287cad84b4295b82456dae7793637e6f68
Anna Kozlovskaia <amorelleth@gmail.com>
2024-06-08 17:54:53 +0200

    feat: frontend: refactor launches fetching

--- frontend/src/api/launches.ts
+++ frontend/src/api/launches.ts
@@ -1,55 +1,53 @@
-import { useFetch } from "./fetch";
-
 export type Launch = {
   flight_number: number;
   name?: string;
   date_utc: string;
-  date_unix: number;
-  date_local: string;
-  date_precision: "half" | "quarter" | "year" | "month" | "day" | "hour";
-  static_fire_date_utc?: string;
-  static_fire_date_unix?: number;
-  tdb: boolean;
-  net: boolean;
-  window?: number;
+  // date_unix: number;
+  // date_local: string;
+  // date_precision: "half" | "quarter" | "year" | "month" | "day" | "hour";
+  // static_fire_date_utc?: string;
+  // static_fire_date_unix?: number;
+  // tdb: boolean;
+  // net: boolean;
+  // window?: number;
   rocket?: string;
   success?: boolean;
-  failures: {
-    time?: number;
-    altitude?: number;
-    reason?: string;
-  }[];
+  // failures: {
+  //   time?: number;
+  //   altitude?: number;
+  //   reason?: string;
+  // }[];
   upcoming: boolean;
   details?: string;
 
-  fairings: {
-    reused?: boolean;
-    recovery_attempt?: boolean;
-    recovered?: boolean;
-    ships: string[];
-  };
+  // fairings: {
+  //   reused?: boolean;
+  //   recovery_attempt?: boolean;
+  //   recovered?: boolean;
+  //   ships: string[];
+  // };
 
-  crew: {
-    crew?: string;
-    role?: string;
-  }[];
+  // crew: {
+  //   crew?: string;
+  //   role?: string;
+  // }[];
 
-  ships: string[];
-  capsules: string[];
-  payloads: string[];
-  launchpad?: string;
+  // ships: string[];
+  // capsules: string[];
+  // payloads: string[];
+  // launchpad?: string;
 
-  cores: {
-    core?: string;
-    flight?: number;
-    gridfins?: boolean;
-    legs?: boolean;
-    reused?: boolean;
-    landing_attempt?: boolean;
-    landing_success?: boolean;
-    landing_type?: string;
-    landpad?: string;
-  }[];
+  // cores: {
+  //   core?: string;
+  //   flight?: number;
+  //   gridfins?: boolean;
+  //   legs?: boolean;
+  //   reused?: boolean;
+  //   landing_attempt?: boolean;
+  //   landing_success?: boolean;
+  //   landing_type?: string;
+  //   landpad?: string;
+  // }[];
 
   links?: {
     patch?: {
@@ -72,12 +70,55 @@ export type Launch = {
     article?: string;
     wikipedia?: string;
   };
-  auto_update: boolean;
+  // auto_update: boolean;
 };
 
-type Response = Launch[];
+export type Response = {
+  launches: Launch[];
+  totalDocs: number;
+  limit: number;
+  totalPages: number;
+  page: number;
+  pagingCounter: number;
+  hasPrevPage: boolean;
+  hasNextPage: boolean;
+  prevPage: number;
+  nextPage: number;
+};
 
-export const useLaunches = () =>
-  useFetch<Response>(() =>
-    fetch("http://localhost:3000/api/launches").then((r) => r.json())
-  );
+export const fetchLaunches = ({
+  limit = 3,
+  page = 1,
+}: {
+  limit?: number;
+  page?: number;
+} = {}) => {
+  return fetch("http://localhost:3000/api/launches", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        limit,
+        page,
+        // sort { Object | String } - Sort order. Documentation
+        pagination: true,
+        select: [
+          "name",
+          "details",
+          "date_utc",
+          "rocket",
+          "success",
+          "details",
+          "flight_number",
+          "upcoming",
+          "links",
+        ],
+      },
+    }),
+  })
+    .then((r) => r.json())
+    .then(({ docs, ...rest }) => ({ launches: docs, ...rest }));
+};
