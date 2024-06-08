878142e2fbcf30b4570477d634799bb2171f05ef
Anna Kozlovskaia <amorelleth@gmail.com>
2024-06-09 01:30:09 +0200

    feat: frontend: add loader

--- frontend/src/pages/main/Main.tsx
+++ frontend/src/pages/main/Main.tsx
@@ -7,27 +7,31 @@ import { FiltersToolbar, type Filters } from "./Filters";
 
 import { getParams } from "./getParams";
 
+import styles from "./Main.module.css";
+
 export const Main = () => {
+  const [filters, setFilters] = useState<Filters>({});
+
+  return (
+    <>
+      <FiltersToolbar value={filters} onChange={setFilters} />
+      <Content filters={filters} />
+    </>
+  );
+};
+
+const Content = ({ filters }: { filters: Filters }) => {
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
 
   const [launches, setLaunches] = useState<Launch[]>([]);
   const [hasMore, setHasMore] = useState(false);
 
-  const [filters, setFilters] = useState<Filters>({});
-
-  // const [isError, setIsError] = useState(false);
-
   const fetchData = useCallback(async () => {
-    try {
-      const data = await fetchLaunches(getParams(page, filters));
-      setHasMore(data.hasNextPage);
-      setLaunches((launches) => [...launches, ...data.launches]);
-    } catch {
-      // setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
+    const data = await fetchLaunches(getParams(page, filters));
+    setHasMore(data.hasNextPage);
+    setLaunches((launches) => [...launches, ...data.launches]);
+    setIsLoading(false);
   }, [page, filters]);
 
   useEffect(() => {
@@ -47,20 +51,15 @@ export const Main = () => {
     setIsLoading(true);
   }, []);
 
-  // if (isLoading) return "Loading...";
-  // if (isError) return "Error...";
-
-  // if (!launches.length) return "Empty";
-
   return (
     <>
-      <FiltersToolbar value={filters} onChange={setFilters} />
       <Launches
         value={launches}
         hasMore={hasMore}
         isLoading={isLoading}
         loadMore={loadMore}
       />
+      {isLoading && <div className={styles.loader}>Loading</div>}
     </>
   );
 };
