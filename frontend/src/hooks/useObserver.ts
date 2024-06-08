e859474faf693ff89ad279be21ba3f36401f794b
Anna Kozlovskaia <amorelleth@gmail.com>
2024-06-08 17:53:56 +0200

    feat: frontend: add useObserver hook

---
+++ frontend/src/hooks/useObserver.ts
@@ -0,0 +1,25 @@
+import { useRef, useCallback } from "react";
+
+export const useObserver = (
+  callback: () => void,
+  options?: IntersectionObserverInit
+) => {
+  const observer = useRef<IntersectionObserver>();
+
+  const ref = useCallback(
+    (node: HTMLElement | null) => {
+      if (observer.current) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          callback();
+        }
+      }, options);
+
+      if (node) observer.current.observe(node);
+    },
+    [callback, options]
+  );
+
+  return ref;
+};
