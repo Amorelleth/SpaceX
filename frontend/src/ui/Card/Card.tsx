aa58e0541b07734e57f33e1898c61af88cda77bc
Anna Kozlovskaia <amorelleth@gmail.com>
2024-06-08 11:27:19 +0200

    feat: frontend: add Card ui component

---
+++ frontend/src/ui/Card/Card.tsx
@@ -0,0 +1,33 @@
+import type { ReactNode } from "react";
+import { clsx } from "clsx";
+
+import { LazyImage } from "../LazyImage";
+
+import styles from "./Card.module.css";
+
+export const Card = ({
+  title,
+  content,
+  image,
+  onClick,
+}: {
+  title?: ReactNode;
+  content: ReactNode;
+  image?: { small?: string; large?: string; alt: string };
+  onClick?: () => void;
+}) => {
+  return (
+    <div
+      className={clsx(styles.container, { [styles.clickable]: onClick })}
+      tabIndex={onClick ? 0 : -1}
+    >
+      {image ? (
+        <LazyImage className={styles.image} {...image} />
+      ) : (
+        <div className={styles.stub} />
+      )}
+      <h3 className={styles.title}>{title}</h3>
+      <div className={styles.content}>{content}</div>
+    </div>
+  );
+};
