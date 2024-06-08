f53e4e217c87f09b56c6cea54cd477a6ebf50d23
Anna Kozlovskaia <amorelleth@gmail.com>
2024-06-08 18:11:58 +0200

    chore: frontend: rm strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { App } from "./components/App";
 
 import "./variables.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
