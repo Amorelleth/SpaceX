f2945d6005b3b3c995575e0a361ddf42369b927a
Anna Kozlovskaia <amorelleth@gmail.com>
2024-06-09 13:38:42 +0200

    chore: frontend: build with vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    target: "es6",
+    emptyOutDir: true,
+    outDir: "dist",
+  },
+  plugins: [react(), dts()],
+});
