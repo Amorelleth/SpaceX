111ba4755ad863bd823b16697ec50135d466d143
Anna Kozlovskaia <amorelleth@gmail.com>
2024-06-09 18:33:08 +0200

    feat: frontend: add comments to main component

--- frontend/src/pages/main/Main.tsx
+++ frontend/src/pages/main/Main.tsx
@@ -9,8 +9,11 @@ import { fuzzySearch } from "./search";
 
 import styles from "./Main.module.css";
 
+// Main page component
 export const Main = () => {
+  // Backend filters state (except fuzzy search)
   const [filters, setFilters] = useState<Filters>({});
+  // Fuzzy search state
   const [search, setSearch] = useState<string>();
 
   return (
@@ -26,6 +29,7 @@ export const Main = () => {
   );
 };
 
+// Content component fetching and displaying launch data
 const Content = ({
   filters,
   search,
@@ -36,12 +40,14 @@ const Content = ({
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-
-  const [launches, setLaunches] = useState<Launch[]>([]);
-  const [searchedLaunches, setSearchedLaunches] = useState<Launch[]>([]);
-
   const [hasMore, setHasMore] = useState(false);
 
+  // State for storing fetched launches
+  const [launches, setLaunches] = useState<Launch[]>([]);
+  // State for storing launches after applying search filter
+  const [searchedLaunches, setSearchedLaunches] = useState<Launch[]>([]);
+
+  // Function to fetch launch data
   const fetchData = useCallback(async () => {
     try {
       const data = await fetchLaunches({ page, ...filters });
@@ -54,23 +60,28 @@ const Content = ({
     }
   }, [page, filters]);
 
+  // Fetching data when loading state changes
   useEffect(() => {
     if (isLoading) {
       fetchData();
     }
   }, [isLoading, fetchData]);
 
+  // Effect to reset launches and page number when filters change
   useEffect(() => {
     setLaunches([]);
     setPage(1);
+    // Trigger refetch
     setIsLoading(true);
   }, [filters]);
 
+  // Function to load more data (increment page number)
   const loadMore = useCallback(() => {
     setPage((page) => page + 1);
     setIsLoading(true);
   }, []);
 
+  // Applying fuzzy search on launches when search query or fetched launches change
   useEffect(() => {
     setSearchedLaunches(search ? fuzzySearch(launches, search) : launches);
   }, [search, launches]);
